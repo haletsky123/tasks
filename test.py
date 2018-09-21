4fff05371dce444b3a3fde0a8da10c04fcca4c0b
Владислав Кульбашный <vladagron@gmail.com>
2018-09-21 23:35:08 +0300

    RAM to XDB
    added modules and programm to convert RAM to XDB

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 from xdb_to_ram import *
+from ram_to_xdb import *
 file = os.path.abspath('materials/tasks.xdb')
 xtr =  XdbToRam(file)
-xtr.parse();
+xtr.parse()
 print(xtr.schema.description)
-for x in xtr.schema.tables:
-    print(x.description)
+for table in xtr.schema.tables:
+    print (table.description)
+
+file = os.path.abspath('materials') + '/tasks1.xdb'
+rtx = RamToXdb(file ,xtr.schema)
+rtx.generate()
