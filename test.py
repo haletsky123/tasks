e38664200367e8d192d01ac0411fb651a6f126a8
Владислав Кульбашный <vladagron@gmail.com>
2018-09-22 12:13:35 +0300

    Ram to Dbd v.0.9

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from xdb_to_ram import *
 from ram_to_xdb import *
+from ram_to_dbd import *
 file = os.path.abspath('materials/tasks.xdb')
 xtr =  XdbToRam(file)
 xtr.parse()
@@ -7,6 +8,9 @@ print(xtr.schema.description)
 for table in xtr.schema.tables:
     print (table.description)
 
-file = os.path.abspath('materials') + '/tasks1.xdb'
-rtx = RamToXdb(file ,xtr.schema)
-rtx.generate()
+# file = os.path.abspath('materials') + '/tasks1.xdb'
+# rtx = RamToXdb(file ,xtr.schema)
+# rtx.generate()
+
+file = os.path.abspath('materials') + '/tasks.dbd'
+rtd = RamToDbd (file, xtr.schema)
